9db51957e5088c469847e213b5edfc6a6149f773
Whiteknight <wknight8111@gmail.com>
2010-11-14 21:02:57 -0500

    +stuff

---
+++ _includes/snippets.js
@@ -0,0 +1,5 @@
+snippets = [
+"All your bytecode are belong to Parrot",
+"Where your javascript goes to die",
+"Momma called the doctor and the doctor said, 'it's the new millenium, stop using FORTRAN'"
+];
