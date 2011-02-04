6f8f8eec99eb228ebb3e0bc2ead8333c2972f8f7
Whiteknight <wknight8111@gmail.com>
2011-02-04 09:36:19 -0500

    +more inspirational snippets

--- _includes/snippets.js
+++ _includes/snippets.js
@@ -1,5 +1,11 @@
 snippets = [
-"All your bytecode are belong to Parrot",
 "Momma called the doctor and the doctor said, 'it's the new millenium, stop using FORTRAN'",
-"It's 8:00PM. Are your children properly encapsulated?"
+"It's 8:00PM. Are your children properly encapsulated?",
+"Best practices? No, we have our own unique way of using Subversion.",
+"Fancy terms like 'patterns', 'unit tests' and 'abstraction' aren't used around here",
+"There's the right way, the wrong way, and our way. Do it our way.",
+"No need to budget time for testing and debugging. Just write it correctly the first time!",
+"I've got some really great ideas. All I need are some technical people to put the code together",
+"All programming languages are the same: Symbols and gibberish. Just pick one and stick with it."
+
 ];
