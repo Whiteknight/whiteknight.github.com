2d327cc73081b517fb04da394ecab08a388ba12f
Andrew Whitworth <wknight8111@gmail.com>
2015-01-14 13:40:22 -0500

    Update snippets.js
    add a few more little tidbits

--- _includes/snippets.js
+++ _includes/snippets.js
@@ -7,5 +7,8 @@ snippets = [
 "No need to budget time for testing and debugging. Just write it correctly the first time!",
 "I've got some really great ideas. All I need are some technical people to put the code together",
 "All programming languages are the same: Symbols and gibberish. Just pick one and stick with it.",
-"Of course DataContextSessionContextRelationFactory implements IDataContextSessionContextRelationFactory!"
+"Of course DataContextSessionContextRelationFactory implements IDataContextSessionContextRelationFactory!",
+"Optimize? We can just add another box to the cluster",
+"If we aren't going to meet the deadline, we can just bring in some of the guys from the other team to help",
+"I can just validate the URL with a regular expression! It will be easy."
 ];
